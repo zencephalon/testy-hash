53f8c404afe893e6119c7f32156981cbad241ab8
Matthew Bunday <zenceph@gmail.com>
2014-05-14 13:09:06 -0400

    More progress.

--- testy_hash.rb
+++ testy_hash.rb
@@ -9,19 +9,29 @@ class SimpleHash
     @store = Array.new(size, [])
   end
 
+  def find_key(key, arr)
+    arr.each do |k, v|
+      return v if k == key
+    end
+    return nil
+  end
+
   def [](key)
     store_key = key % @size
     if @store[store_key].empty?
       return nil
     else
-      find_key(key, @store[store_key])
+      return find_key(key, @store[store_key])
     end
   end
 
   def []=(key, value)
+    store_key = key % @size
     @store[store_key] << [key, value]
   end
 end
 
 hash = SimpleHash.new(50)
+hash[3] = 5
 assert_equal(nil, hash[5])
+assert_equal(5, hash[3])
