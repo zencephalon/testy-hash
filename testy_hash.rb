d960cf4da8ebd485bf3209f82f756d56d306f387
Matthew Bunday <zenceph@gmail.com>
2014-05-14 12:53:32 -0400

    Begin SimpleHash implementation.

--- testy_hash.rb
+++ testy_hash.rb
@@ -15,13 +15,30 @@ def assert_equal(expected, test)
     if expected.nil?
       expected = 'nil'
     end
-    
+
     puts "Failed! Expected #{expected} but got #{test}".red
   end
 end
 
-assert(25 == 5 * 5)
-assert(25 == "5" * 5)
-assert_equal(25, 5 * 5)
-assert_equal(25, "5" * 5)
-assert_equal(nil, [1, 2][1])
+class SimpleHash
+  attr_accessor :size
+
+  def initialize(size)
+    @size = size
+    @store = Array.new(5, [])
+  end
+
+  def [](key)
+    5
+  end
+
+  def []=(key, value)
+    puts 5
+    5
+  end
+end
+
+hash = SimpleHash.new(50)
+assert_equal(5, hash[5])
+assert_equal(5, hash[3] = 7)
+assert_equal(5, hash.[]=(3, 7))
