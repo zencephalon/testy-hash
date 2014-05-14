f71161e0cd985debfdd9227d4ae9db7a9bb42637
Matthew Bunday <zenceph@gmail.com>
2014-05-14 12:44:29 -0400

    Added assert_equal

--- testy_hash.rb
+++ testy_hash.rb
@@ -4,9 +4,20 @@ def assert(bool)
   if bool
     puts "Test passed!".green
   else
-    puts "Failed! Expected true.".red
+    puts "Failed! Expected true".red
+  end
+end
+
+def assert_equal(expected, test)
+  if expected == test
+    puts "Test passed!".green
+  else
+    puts "Failed! Expected #{expected} but got #{test}".red
   end
 end
 
 assert(25 == 5 * 5)
 assert(25 == "5" * 5)
+assert_equal(25, 5 * 5)
+assert_equal(25, "5" * 5)
+assert_equal(nil, [1, 2][1])
